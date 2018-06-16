38b8fb475dbef1db673a77ca2b677ca1decff4f0
serinuntius <wasab1gj@gmail.com>
2018-06-16 13:13:00 +0900

    exportする

--- fluentd/env.sh
+++ fluentd/env.sh
@@ -1,2 +1,2 @@
-AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
+export AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
